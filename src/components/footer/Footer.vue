97bee482204e353517f5ae04021e9fef16bc8510
codey4k <codey4k@gmail.com>
2018-03-10 17:48:43 +0000

    create Footer component

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+
+<footer class="footer">
+</footer>
+
+</template>
+
+<script>
+
+export default {
+    name : 'Footer'
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+.footer {
+    width : 100%;
+    height : 200px;
+    background : var(--theme);
+}
+
+</style>
