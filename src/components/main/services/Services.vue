5150b27200c5420476de51225a7615f09cd31e84
codey4k <codey4k@gmail.com>
2018-03-21 23:55:56 +0000

    edit Services component

--- src/components/main/services/Services.vue
+++ src/components/main/services/Services.vue
@@ -1,6 +1,9 @@
 <template>
     <section class="services">
-        <div class="services__wrap"></div>
+        <div class="services__wrap">
+            <h2 class="services__title">Features</h2>
+            <p>dgjidfg idnfgi isdfsdfsd isdfisdfj sdfijsdifsdfijsd s dfjisjsdf ji j isdjfi ijdsfijisdfjiisdifi</p>
+        </div>
     </section>
 </template>
 
@@ -13,13 +16,24 @@ export default {
 <style lang="scss" scoped>
 
 .services {
+    display : flex;
     width : 100%;
     height : 200px;
-    background : tomato;
+    background : transparent;
     margin-top : -50px;
     position : relative;
     
-    &__wrap {}
+    &__wrap {
+        margin : auto;
+        display : flex;
+        flex-direction : column;
+        align-items : center;
+        justify-content : center;
+    }
+    &__title {
+        color : #666;
+        padding : .8em;
+    }
 }
 
 </style>
