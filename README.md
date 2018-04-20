ff12f1e07756ba4bf07e02512caf36bd38745502
codey4k <codey4k@gmail.com>
2018-04-20 22:42:32 +0000

    update 20/04/2018

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
-# app
+# App Vue
 
-> A Vue.js project
+> by @codey4k
 
-## Build Setup
+## Run
 
 ``` bash
-# install dependencies
-npm install
-
 # serve with hot reload at localhost:8080
 npm run dev
-
-# build for production with minification
-npm run build
 ```
-
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/main/feature/Feature.vue
@@ -0,0 +1,40 @@
+<template>
+    <section class="feature">
+        <div class="feature__wrap">
+            <h2 class="feature__title">Features</h2>
+            <p>dgjidfg idnfgi isdfsdfsd isdfisdfj sdfijsdifsdfijsd s dfjisjsdf ji j isdjfi ijdsfijisdfjiisdifi</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name : 'Feature'
+}
+</script>
+
+<style lang="scss" scoped>
+
+.feature {
+    display : flex;
+    width : 100%;
+    height : 200px;
+    background : #ddd;
+    margin-top : -50px;
+    position : relative;
+    z-index : 1000;
+    
+    &__wrap {
+        margin : auto;
+        display : flex;
+        flex-direction : column;
+        align-items : center;
+        justify-content : center;
+    }
+    &__title {
+        color : #666;
+        padding : .8em;
+    }
+}
+
+</style>
